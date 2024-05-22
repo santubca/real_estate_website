a42bec272484cd1824dba98038ac7f5a9ef2f4f2
Santu Maity <santu7maity@gmail.com>
2024-05-23 00:29:43 +0530

    23-05-2024_add some line of code

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,15 @@ export default {
       fontSize:{
         sm:"0.75rem",
         base:"0.87rem",
+        baseb:"0.90rem",
         md:"1rem",
         lg:"1.25rem",
         xl:"1.5rem",
         "2xl":"1.75rem",
         "3xl":"2.25rem",
-      }
+      },
+      backgroundImage:{
+        'team-bg-image': "url('/public/team/team.png')",      }
     },
   },
   plugins: [],
