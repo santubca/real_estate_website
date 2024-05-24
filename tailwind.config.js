d8f8eda1768be89ee723480edccdba3ae2e46252
Santu Maity <santu7maity@gmail.com>
2024-05-25 03:16:52 +0530

    25-05-2024_responsive_90%_done

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,13 @@ export default {
         baseb:"0.90rem",
         md:"1rem",
         lg:"1.25rem",
+        blg:"1.125rem",
+        bblg:"1.375",
         xl:"1.5rem",
         "2xl":"1.75rem",
         "3xl":"2.25rem",
+        "3xxl":"2.875rem",
+        "4xl":"3rem",
       },
       backgroundImage:{
         'team-bg-image': "url('/public/team/team.png')",      }
